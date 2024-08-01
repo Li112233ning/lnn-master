61e832a80d83d0f79e7fa6cbfd7d89490e6409dc
lnn <3238773417@qq.com>
2024-08-01 22:13:29 +0800

    1

---
+++ src/test/java/com/lnn/LnnMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LnnMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
